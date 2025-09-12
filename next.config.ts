9e2426699190b37bf77c918c53f78b5a044ec15c
Bora <boraaaydin@gmail.com>
2025-09-12 15:49:04 +0300

    assetPrefix added

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/blog' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/blog' : '',
+  basePath: isProd ? '/blog' : '',
+  assetPrefix: isProd ? '/blog' : '',
   images: {
     unoptimized: true,
   },
